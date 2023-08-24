033dedb01f5177823fa0a24fd80e3931ac622c84
tomneozed <tomneozed@gmail.com>
2023-08-24 10:18:19 +0200

    Init project

---
+++ src/main/java/com/example/TPIntermediaireFavoris/TpIntermediaireFavorisApplication.java
@@ -0,0 +1,13 @@
+package com.example.TPIntermediaireFavoris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpIntermediaireFavorisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpIntermediaireFavorisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TPIntermediaireFavoris/TpIntermediaireFavorisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TPIntermediaireFavoris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpIntermediaireFavorisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
