4513e0b3486320eb29f6895fc92a7a36adb849be
tomneozed <tomneozed@gmail.com>
2023-08-29 09:29:35 +0200

    CRUD Favorite - Remove Delete, use DeleteMultiple instead

--- src/main/java/com/example/TPIntermediaireFavoris/controller/FavoriteController.java
+++ src/main/java/com/example/TPIntermediaireFavoris/controller/FavoriteController.java
@@ -4,7 +4,6 @@ import com.example.TPIntermediaireFavoris.dto.FavoriteDTO;
 import com.example.TPIntermediaireFavoris.service.IFavoriteService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -32,21 +31,9 @@ public class FavoriteController {
         return favoriteService.findOne(id);
     }
 
-    @Operation(summary = "Delete a Favorite by its ID")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "The Favorite was deleted"),
-            @ApiResponse(responseCode = "404", description = "The Favorite was not found"),
-            @ApiResponse(responseCode = "500", description = "The Favorite couldn't be deleted")
-    })
-    @DeleteMapping(path = "/{id}")
-    @ResponseStatus(code = HttpStatus.OK)
-    void delete(@PathVariable long id) {
-        favoriteService.delete(id);
-    }
-
     @Operation(summary = "Delete multiple Favorites by their IDs")
     @ApiResponse(responseCode = "200", description = "The Favorite was deleted")
-    @DeleteMapping(path = "/favorite/{ids}")
+    @DeleteMapping(path = "/{ids}")
     @ResponseStatus(code = HttpStatus.OK)
     void deleteMultiple(@PathVariable String ids) {
         favoriteService.deleteMultiple(
